f118f3dc0a796297b55812244f38eb5614b2a051
devi-debug <itsomm143@gmail.com>
2026-08-24 20:50:22 +0530

    added the styler app to start as a expo app rather than starting as react native CLI

---
+++ styler02-expo/App.tsx
@@ -0,0 +1,24 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FlatCards from './components/FlatCards'
+import ElevatedCards from './components/ElevatedCards'
+import FancyCard from './components/FancyCard'
+import ActionCard from './components/ActionCard'
+import ContactList from './components/ContactList'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards />
+        <FancyCard />
+        <ContactList />
+        <ActionCard />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
